6853285ce1d8f54555a4e66537ba6ca616da1e4a
Christian <christiansmith2394@gmail.com>
2022-05-09 19:05:17 -0400

    updated location on embedded map

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -13,10 +13,17 @@ function ContactPage() {
             </div>
             <div className="ContactPage">
                 <div className="map-sect">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.700327595181!2d-0.13858868403737226!3d50.836714467337785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487585104ec1fe1b%3A0x28b5349b15ce0c4d!2sLondon%20Road%20(Brighton)%20Train%20Station%20-%20Southern%20Railway!5e0!3m2!1sen!2suk!4v1611468671158!5m2!1sen!2suk" width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24685.490313746144!2d-74.5000455461547!3d39.34069258828403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c0c2d67d7f1a7b%3A0x617b4e170c6d5854!2sVentnor%20City%2C%20NJ%2C%20USA!5e0!3m2!1sen!2suk!4v1652137379881!5m2!1sen!2suk" 
+                    width="600" 
+                    height="450" 
+                    frameBorder="0" 
+                    style={{border:0}} 
+                    allowFullScreen="" 
+                    aria-hidden="false" 
+                    tabIndex="0"></iframe>
                 </div>
                 <div className="contact-sect">
-                    <ContactItem icon={phone} text1={'+1 609-224-7221'} text2={'+1 609-224-7221'} title={'Phone'}/>
+                    <ContactItem icon={phone} text1={'+1 609-224-7221'} title={'Phone'}/>
                     <ContactItem icon={email} text1={'christiansmith2394@gmail.com'} title={'Email'}/>
                     <ContactItem icon={location} text1={'Ventnor City, New Jersey'} text2={'United States'} title={'Location'}/>
                 </div>
