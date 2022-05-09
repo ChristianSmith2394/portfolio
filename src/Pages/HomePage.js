206497dbccf54848404d7560527828904634a6eb
Christian <christiansmith2394@gmail.com>
2022-05-09 18:57:43 -0400

    updated contact info page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -9,13 +9,10 @@ function HomePage() {
             <header className="hero">
                 <h1 className="hero-text">
                     Hi, I am 
-                    <span> Lorem Ipsum.</span>
+                    <span> Christian Smith.</span>
                 </h1>
                 <p className="h-sub-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Cupiditate facere, numquam vitae placeat consequatur corrupti officia 
-                    quibusdam. Blanditiis doloremque nemo ex facilis neque. 
-                    A sint ipsam earum nemo omnis et.
+                    Welcome to my portfolio page where you can see all the projects I have worked on as well as projects I'm currently working on!
                 </p>
                 <div className="icons">
                     {/* <Link to="https://github.com/ChristianSmith2394" className="icon-holder">
