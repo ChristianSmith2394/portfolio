332e24d69f10f6b645887038c9bfaad8de98d195
Christian <christiansmith2394@gmail.com>
2022-12-14 09:13:14 -0500

    Updated portfolio pieces

--- src/Components/allportfolios.js
+++ src/Components/allportfolios.js
@@ -1,53 +1,65 @@
-import port1 from '../img/portImages/AmazonClone.png';
-import port2 from '../img/portImages/NetflixClone.png';
-import port3 from '../img/portImages/Calculator.png'
-import port4 from '../img/portImages/Todo.png'
+import port1 from "../img/portImages/AmazonClone.png";
+import port2 from "../img/portImages/NetflixClone.png";
+import port3 from "../img/portImages/Calculator.png";
+import port4 from "../img/portImages/Todo.png";
+import port5 from "../img/portImages/Flights.png";
 
 const portfolios = [
-    {
-        id: 1,
-        category: 'React Js',
-        link1: 'https://ecom-store-cs94.web.app/',
-        link2: 'https://github.com/ChristianSmith2394/ecom-store',
-        icon1: 'Website',
-        icon2: 'GitHub',
-        image: port1,
-        title: 'Amazon Clone',
-        description: 'A fully functioning Amazon reproduction'
-    },
-    {
-        id: 2,
-        category: 'React Js',
-        link1: 'https://netflix-clone-2374a.web.app/',
-        link2: 'https://github.com/ChristianSmith2394/netflix-clone',
-        icon1: 'Website',
-        icon2: 'GitHub',
-        image: port2,
-        title: 'Netflix Clone',
-        description: `A representation of Netflix's homepage`
-    },
-    {
-        id: 3,
-        category: 'React Js',
-        link1: '',
-        link2: 'https://github.com/ChristianSmith2394/calculator',
-        icon1: 'Website',
-        icon2: 'GitHub',
-        image: port3,
-        title: 'Calculator',
-        description: 'A simple stylish calculator'
-    },
-    {
-        id: 4,
-        category: 'React Js',
-        link1: '',
-        link2: 'https://github.com/ChristianSmith2394/to-do',
-        icon1: 'Website',
-        icon2: 'GitHub',
-        image: port4,
-        title: 'Todo App',
-        description: 'A simple todo application'
-    }
-]
+  {
+    id: 1,
+    category: "React Js",
+    link1: "https://ecom-store-cs94.web.app/",
+    link2: "https://github.com/ChristianSmith2394/ecom-store",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port1,
+    title: "Amazon Clone",
+    description: "A fully functioning Amazon reproduction",
+  },
+  {
+    id: 2,
+    category: "React Js",
+    link1: "https://netflix-clone-2374a.web.app/",
+    link2: "https://github.com/ChristianSmith2394/netflix-clone",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port2,
+    title: "Netflix Clone",
+    description: `A representation of Netflix's homepage`,
+  },
+  {
+    id: 3,
+    category: "React Js",
+    link1: "",
+    link2: "https://github.com/ChristianSmith2394/calculator",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port3,
+    title: "Calculator",
+    description: "A simple stylish calculator",
+  },
+  {
+    id: 4,
+    category: "React Js",
+    link1: "",
+    link2: "https://github.com/ChristianSmith2394/to-do",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port4,
+    title: "Todo App",
+    description: "A simple todo application",
+  },
+  {
+    id: 5,
+    category: "React Js",
+    link1: "",
+    link2: "https://github.com/CKT-Technologies/ckt-flights",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port5,
+    title: "Todo App",
+    description: "A simple todo application",
+  },
+];
 
 export default portfolios;
